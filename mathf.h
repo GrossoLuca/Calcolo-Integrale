997c99ab4a22f5a10ab479fc095239161830eba6
Luca Grosso <grossolu@icloud.com>
2024-02-29 09:44:09 +0100

    first commit

---
+++ mathf.h
@@ -0,0 +1,8 @@
+#ifndef MATHF_H
+#define MATHF_H
+
+float Polynomial(float* coeff, int size, float in);
+void Rectangular(float* values, int size, float stepsize, float* integ1, float* integ2);
+float Trapezoidal(float* values, int size, float stepsize);
+
+#endif
